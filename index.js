83f1701d5defdbb94904511eeab853958880be42
raduanakhawla <raduanakhawla468@gmail.com>
2021-12-15 22:43:17 +0600

    server side

--- index.js
+++ index.js
@@ -24,12 +24,13 @@ async function run() {
       const postCollection = client.db("codeCollection").collection("post");
       const usersCollection = client.db("codeCollection").collection("user");
 
-      // get all services
+      // get all Posts
       app.get("/allPosts", async (req, res) => {
         const result = await postCollection.find({}).toArray();
         res.json(result);
       });
-      // single service
+
+      // single Post
       app.get("/singleService/:id", async (req, res) => {
         console.log(req.params.id);
         const result = await postCollection
@@ -43,53 +44,82 @@ async function run() {
         const result = await postCollection.insertOne(req.body);
         res.json(result);
       });
-      //  make admin
+      app.get("/users/:email", async (req, res) => {
+        const email = req.params.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        let isAdmin = false;
+        if (user?.role === "admin") {
+          isAdmin = true;
+        }
+        res.json({ admin: isAdmin });
+      });
 
-      app.put("/makeAdmin", async (req, res) => {
-        const filter = { email: req.body.email };
-        const result = await usersCollection.find(filter).toArray();
-        if (result) {
-          const documents = await usersCollection.updateOne(filter, {
-            $set: { role: "admin" },
+      app.post("/users", async (req, res) => {
+        const user = req.body;
+        const result = await usersCollection.insertOne(user);
+        console.log(result);
+        res.json(result);
+      });
+
+      app.put("/users", async (req, res) => {
+        const user = req.body;
+        const filter = { email: user.email };
+        const options = { upsert: true };
+        const updateDoc = { $set: user };
+        const result = await usersCollection.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
+        res.json(result);
+      });
+
+      app.put("/users/admin", async (req, res) => {
+        const user = req.body;
+        const requester = req.decodedEmail;
+        if (requester) {
+          const requesterAccount = await usersCollection.findOne({
+            email: requester,
           });
-          console.log(documents);
+          if (requesterAccount.role === "admin") {
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: "admin" } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+          }
+        } else {
+          res
+            .status(403)
+            .json({ message: "you do not have access to make admin" });
         }
       });
-
-      // check admin or not
-      app.get("/checkAdmin/:email", async (req, res) => {
-        const result = await usersCollection
-          .find({ email: req.params.email })
-          .toArray();
-
-        res.json(result);
-      });
       //order delete
-      app.delete("/deleteOrder/:id", async (req, res) => {
-        const result = await ordersCollection.deleteOne({
-          _id: ObjectId(req.params.id),
-        });
-        // console.log(result);
-        res.json(result);
-      });
-      /// all order
-      app.get("/allOrders", async (req, res) => {
-        // console.log("hello");
-        const result = await ordersCollection.find({}).toArray();
-        res.json(result);
-      });
+      // app.delete("/deleteOrder/:id", async (req, res) => {
+      //   const result = await ordersCollection.deleteOne({
+      //     _id: ObjectId(req.params.id),
+      //   });
+      //   // console.log(result);
+      //   res.json(result);
+      // });
+      // /// all order
+      // app.get("/allOrders", async (req, res) => {
+      //   // console.log("hello");
+      //   const result = await ordersCollection.find({}).toArray();
+      //   res.json(result);
+      // });
 
-      // status update
-      app.put("/statusUpdate/:id", async (req, res) => {
-        const filter = { _id: ObjectId(req.params.id) };
-        console.log(req.params.id);
-        const result = await ordersCollection.updateOne(filter, {
-          $set: {
-            status: req.body.status,
-          },
-        });
-        res.json(result);
-      });
+      // // status update
+      // app.put("/statusUpdate/:id", async (req, res) => {
+      //   const filter = { _id: ObjectId(req.params.id) };
+      //   console.log(req.params.id);
+      //   const result = await ordersCollection.updateOne(filter, {
+      //     $set: {
+      //       status: req.body.status,
+      //     },
+      //   });
+      //   res.json(result);
+      // });
     });
   } finally {
     // await client.close();
